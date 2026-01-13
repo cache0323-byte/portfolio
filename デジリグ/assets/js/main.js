648cdcdf6240eb74c996a58b5270593d554e0e07
Saki.S <cache0323@gmail.com>
2026-01-13 20:17:10 +0900

    113

--- デジリグ/assets/js/main.js
+++ デジリグ/assets/js/main.js
@@ -60,45 +60,39 @@
 }
 
 
-// //voice　スライダー
+//voice　スライダー
 $('.slick-voice').slick({
     mobileFirst: true,
-    autoplay:true,
-    autoplaySpeed: 3000,
+    autoplay: false,
     speed: 600,
-    cssEase:'ease-in-out',
+    cssEase: 'ease-in-out',
     infinite: true,
     slidesToShow: 1, 
-    slidesToScroll:1,
+    slidesToScroll: 1,
     centerMode: true,   
-    centerPadding: '0', 
+    centerPadding: '20px',
     dots: true,
+    arrows: false,
     responsive: [
-        // {
-        //     breakpoint: 1440,
-        //     settings: {
-        //         slidesToShow: 3,
-        //         centerMode: true
-        //     }
-        // },
         {
             breakpoint: 768,
             settings: {
                 slidesToShow: 2,
-                centerMode: false
+                centerMode: false,
             }
         },
         {
             breakpoint: 1024,
             settings: {
                 slidesToShow: 3,
-                slidesToScroll:1,
                 centerMode: true,
-                centerPadding:'0'
+                centerPadding: '0'
             }
         }
     ]
 });
+
+
 
 //studio スライダー　スマホのみ   
 //   $('.slick-studio').slick({
