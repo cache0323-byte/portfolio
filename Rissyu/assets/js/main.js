83d1a94ec5b57f099887bcf47c9f71cf6e7b8079
Saki.S <cache0323@gmail.com>
2026-01-18 15:15:01 +0900

    118

--- Rissyu/assets/js/main.js
+++ Rissyu/assets/js/main.js
@@ -1,60 +1,70 @@
 'use strict';
+//共通のもの
+//画像をふわっとだす
+$(window).on('scroll load', function() {
+  $('.fade-in').each(function() {
+    var target = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll > target - windowHeight + 100) {
+      $(this).addClass('is-active');
+    }
+  });
+});
 
+// ハンバーガーメニュー
 $(function () {
-  // ハンバーガーメニュー
-  $('.menu').on('click', function () {
-    $('.nav').toggleClass('active');
+  // 開くボタン
+  $('.menu_btn').on('click', function () {
+    $('.nav').addClass('active');
   });
 
+  // 閉じるボタン（×ボタン、リンク、背景など）
   $('.nav_btn, .nav_item a, .nav_btnClose').on('click', function () {
     $('.nav').removeClass('active');
   });
+});
 
 
   // VOICE（スマホだけ slick）
-  var $slider = $('.autoplay');
+//   const $slider = $('.autoplay');
 
-  function setVoiceSlider() {
-    var isMobile = window.matchMedia('(max-width: 600px)').matches;
+//   function setVoiceSlider() {
+//     const isMobile = window.matchMedia('(max-width: 600px)').matches;
 
-    if (isMobile) {
-      // スマホ: slick未初期化なら起動
-      if (!.voice_slider.hasClass('slick-initialized')) {
-        $('.voice_slider').slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 3000,
-          dots: true,
-          infinte:true;
-          arrows: true,
-          adaptiveHeight: true
-        });
-      });
-});
+//     if (isMobile) {
+//       // スマホ: slick未初期化なら起動
+//       if ($.voice_slider.hasClass('slick-initialized')) {
+//         $('.voice_slider').slick({
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//           autoplay: true,
+//           autoplaySpeed: 3000,
+//           dots: true,
+//           infinte:true,
+//           arrows: true,
+//           adaptiveHeight: true
+//         });
+//       });
+//     }else {
+//       // PC: slickが動いてたら解除
+//       if (.voice_slider.hasClass('slick-initialized')) {
+//         .voice_slider.slick('unslick');
+//       }
+//     }
+//   setVoiceSlider();
+//   $(window).on('resize orientationchange', setVoiceSlider);
+// });
 
-      }
-    } else {
-      // PC: slickが動いてたら解除
-      if (.voice_slider.hasClass('slick-initialized')) {
-        .voice_slider.slick('unslick');
-      }
-    }
-  }
-
-  setVoiceSlider();
-  $(window).on('resize orientationchange', setVoiceSlider);
-});
-
-$(document).ready(function(){
-    $('.voice_slider').slick({
-    // autoplay: true,      // 自動再生
-    // autoplaySpeed: 3000, // スライドの切り替え時間（ms）
-    // dots: true,          // 下にドット（ページ送り）を表示
-    // arrows: true,        // 左右の矢印を表示
-    // infinite: true,      // 無限ループ
-    // slidesToShow: 1,     // 表示するスライド数
-    // slidesToScroll: 1    // スクロールするスライド数
-    });
-});
+// $(document).ready(function(){
+//     $('.voice_slider').slick({
+//     // autoplay: true,      // 自動再生
+//     // autoplaySpeed: 3000, // スライドの切り替え時間（ms）
+//     // dots: true,          // 下にドット（ページ送り）を表示
+//     // arrows: true,        // 左右の矢印を表示
+//     // infinite: true,      // 無限ループ
+//     // slidesToShow: 1,     // 表示するスライド数
+//     // slidesToScroll: 1    // スクロールするスライド数
+//     });
+// });
 
