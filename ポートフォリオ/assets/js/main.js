03a7b83f9abe2302c6cab545a1e6132f19c2d558
25r1116sasaki-code <25r1116.sasaki@gmail.com>
2026-01-22 17:16:39 +0900

    122

--- ポートフォリオ/assets/js/main.js
+++ ポートフォリオ/assets/js/main.js
@@ -1,14 +1,27 @@
 'use strict';
+//lineのアニメーション
+    document.addEventListener('DOMContentLoaded', () => {
+        const animatedLines = document.querySelectorAll('.animated-line');
 
-{
-    $(function(){
-        $('.header__btn').on('click', function(){
-            $('.nav').toggleClass('active');
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const target = entry.target;
+                    
+                    // 1秒後にクラスを追加
+                    setTimeout(() => {
+                        target.classList.add('is-drawn');
+                    }, 1000); 
+
+                    observer.unobserve(target);
+                }
+            });
+        }, {
+            rootMargin: '0px', 
+            threshold: 0.1 
         });
 
-        $('.nav__btn, .nav__item a').on('click', function(){
-            $('.nav').removeClass('active');
+        animatedLines.forEach(line => {
+            observer.observe(line);
         });
-    });
-
-}
+    })
