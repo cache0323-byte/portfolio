01d0960471c01a4c566a5afd776f3d5901ff69a9
25r1116sasaki-code <25r1116.sasaki@gmail.com>
2026-02-05 09:39:27 +0900

    4枚ProductCard.stories.tsx

--- hair/src/components/molecule/ProductCard.stories.tsx
+++ hair/src/components/molecule/ProductCard.stories.tsx
@@ -25,3 +25,24 @@ export const Default: Story = {
     description: (<>濃密なツヤ<br/>リッチな補修力</>),
   },
 };
+export const Oil02: Story = {
+  args: {
+    imageUrl: '/images/oil02.jpg', // パスは環境に合わせて変更してください
+    title: 'Scalp Vital Oil',
+    description: (<>根本からのハリ<br/>頭皮ケア</>),
+  },
+};
+export const Oil03: Story = {
+  args: {
+    imageUrl: '/images/oil03.jpg', // パスは環境に合わせて変更してください
+    title: 'Balance Moist',
+    description: (<>うねり防止<br/>内側から潤す</>),
+  },
+};
+export const Oil04: Story = {
+  args: {
+    imageUrl: '/images/oil04.jpg', // パスは環境に合わせて変更してください
+    title: 'Airy Silk Oil',
+    description: (<>軽やかな指通り<br/>さらさら感</>),
+  },
+};
